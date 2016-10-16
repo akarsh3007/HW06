505e1eea797b6a4398c2269eb2b7d447690cb3b9
Ahmet Gencoglu <agencogl@uncc.edu>
2016-10-16 13:47:19 -0400

    Favorite City Object Created to hold data

---
+++ app/src/main/java/com/example/akarsh/hw06/FavoriteCity.java
@@ -0,0 +1,41 @@
+package com.example.akarsh.hw06;
+
+public class FavoriteCity {
+
+    private String city, country;
+    private Double temperature;
+    private Boolean isFavorite;
+
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(Double temperature) {
+        this.temperature = temperature;
+    }
+
+    public Boolean getFavorite() {
+        return isFavorite;
+    }
+
+    public void setFavorite(Boolean favorite) {
+        isFavorite = favorite;
+    }
+}
